2cfbad9326ec5a72caef634510b5b2c8cba65ab0
Adrian Anicete <anicete.ian@gmail.com>
2026-04-20 12:07:47 +0800

    feat: setup completed

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to={"/"} className="navbar-brand">
+          ShopHub
+        </Link>
+
+        <div className="navbar-links">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/checkout"}>Cart</Link>
+        </div>
+
+        <div className="navbar-auth">
+          <div className="navbar-auth-links">
+            <Link to={"/auth"} className="btn btn-secondary">
+              Login
+            </Link>
+            <Link to={"/auth"} className="btn btn-primary">
+              SignUp
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
